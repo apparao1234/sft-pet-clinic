5b0fa2305291602bd895ffed74d7f7173fd245c9
apparao <apparao123.g@gmail.com>
2019-12-23 14:08:01 +0530

    updating for auto Id generation.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -28,14 +28,14 @@ public class DataLoader implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
+		//owner1.setId(1L);
 		owner1.setFirstName("Appar");
 		owner1.setLastName("rao");
 		ownerService.save(owner1);
 		
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
+		//owner2.setId(2L);
 		owner2.setFirstName("Appar2");
 		owner2.setLastName("rao2");
 		ownerService.save(owner2);
@@ -43,13 +43,13 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println(owner2);
 		
 		Vet vet1 = new Vet();
-		vet1.setId(3L);
+		//vet1.setId(3L);
 		vet1.setFirstName("Appar3");
 		vet1.setLastName("rao3");
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet2.setId(4L);
+		//vet2.setId(4L);
 		vet2.setFirstName("Appar4");
 		vet2.setLastName("rao4");
 		vetService.save(vet2);
