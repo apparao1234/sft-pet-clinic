93107b4ac5f52fa0f9f870a1e54d70f92a33e9b0
apparao <apparao123.g@gmail.com>
2019-12-24 16:59:49 +0530

    add petType serice map

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService   extends AbstractmapService<PetType,Long> implements PetTypeService {
+
+	
+public Set<PetType> findAll(){
+		
+		return super.findAll();
+	}
+	
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+	
+	public PetType save( PetType object)
+            {
+		return super.save( object)  ;      }
+	
+	public void deleteById(Long id){
+		 super.deleteById(id);
+	}
+	
+	public void delete(PetType object) {
+		super.delete(object);;
+	}
+	
+}
