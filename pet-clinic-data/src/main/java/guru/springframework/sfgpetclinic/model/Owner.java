101297102cc944c50b31c95d2b94f1aecf3b697e
apparao <apparao123.g@gmail.com>
2019-12-24 16:43:51 +0530

    modified Owner to add address info

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -2,7 +2,12 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.util.Set;
 
+
 public class Owner extends Person{
+	
+	private String  adress;
+	private String  city ;
+	private String  telephone;
 	
 	private Set<Pet> pets;
 
